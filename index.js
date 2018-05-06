4f1bb05ea54f22ebd30680a0b1bce1969b87b05e
Lauren Holloway <hello@laurenmholloway.com>
2018-05-06 19:55:32 -0400

    added function to require relationship and added requirement to enter an age

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ addButton.addEventListener("click", validateForm);
 //function to validate age input on form
 function validateAge(){
     var age = document.querySelector("input").value;
-    if(age <= 0){
+    if(age <= 0 || age == ""){
         alert("Age must be greater than 0.");    
+    }
+}
+
+//function to require the relationship input
+function requireRelationship(){
+    var relationship = document.querySelector("select").value;
+    if(relationship == ""){
+        alert("Relationship is required");
     }
 }
 
@@ -13,5 +21,7 @@ function validateAge(){
 function validateForm(){
     //calling function to validate age
     validateAge();
+    //calling function to require relationship
+    requireRelationship();
     
 }
